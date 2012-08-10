56ce15cef96431e0b2b708f7eccfba6789cfa686
Bart Massey <bart@cs.pdx.edu>
2012-08-09 20:31:22 -0700

    continued work on client scripting

---
+++ client/install-minecraft-client.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+DESKTOP=/usr/share/applications/minecraft.desktop
+JAVA=/usr/bin/java
+MINECRAFT='http://www.minecraft.net/download/minecraft.jar'
+
+# If you change these, be sure to change the corresponding
+# definition in minecraft.sh .
+PREFIX=/usr/share
+INSTALL=$PREFIX/minecraft-client
+
+PGM="`basename $0`"
+
