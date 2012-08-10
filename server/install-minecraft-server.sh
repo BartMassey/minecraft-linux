de18a74eea3f726716111e81d847ea7037566f89
Bart Massey <bart@po8.org>
2012-08-09 22:16:55 -0700

    finished cleaning up server install

--- server/install-minecraft-server.sh
+++ server/install-minecraft-server.sh
@@ -12,8 +12,7 @@ CRAFTBUKKIT_URL="http://dl.bukkit.org/latest-rb/craftbukkit.jar"
 
 # If you change these, be sure to change the corresponding
 # definition in minecraft-server.rc .
-PREFIX=/usr/share
-SERVER_DIR=$PREFIX/minecraft-server
+SERVER_DIR=/usr/share/minecraft-server
 
 if [ ! -f "./install-minecraft-server.sh" ]
 then
@@ -21,51 +20,50 @@ then
     exit 1
 fi
 
-if [ ! -x "$PREFIX" ]
-then
-    echo "$PGM: creating $PREFIX" >&2
-    mkdir "$PREFIX" || exit 1
-fi
-
 if [ -x "$SERVER_DIR" ]
 then
     echo "$PGM: cowardly refusing since $SERVER_DIR exists." >&2
     exit 1
 fi
-mkdir "$SERVER_DIR" || exit 1
 
-( cd "$SERVER_DIR" &&
-  
+MC_HOME="`bash -c \"echo ~minecrft\"`"
+case "$MC_HOME" in
+    '~minecrft')
+	adduser --gecos 'Minecraft Server' --home "$SERVER_DIR" \
+	  --disabled-password minecrft
+	;;
+    "$SERVER_DIR")
+	;;
+    *)
+	usermod -d "$SERVER_DIR" minecrft
+	;;
+esac
+
+[ ! -d "$SERVER_DIR" ] && mkdir "$SERVER_DIR"
+chown minecrft "$SERVER_DIR"
 
 cp minecraft-server.sh "$SERVER_DIR"/
 chmod +x "$SERVER_DIR"/minecraft-server.sh
 
-cp minecraft-server.rc /etc/init.d/minecraft-server
+sed "s#@SERVER_DIR@#$SERVER_DIR#g" \
+    minecraft-server.rc >/etc/init.d/minecraft-server
 chmod +x /etc/init.d/minecraft-server
 update-rc.d minecraft-server enable
-
-MC_HOME="`bash -c \"echo ~minecrft\"`"
-case "$MC_HOME" in
-    $SERVER_DIR) ;;
-    "") adduser --gecos 'Minecraft Server' --home "$SERVER_DIR" \
-          --disabled-password minecrft ;;
-    *) usermod -d "$SERVER_DIR" minecrft ;;
-esac
 
 cd "$SERVER_DIR"
 mkdir .ssh
 ( cd .ssh &&
   chmod 700 . &&
-  ssh-keygen -p '' -t rsa -b 2048 -f id-rsa-minecraft &&
+  ssh-keygen -N '' -t rsa -b 2048 -f id-rsa-minecraft &&
   mv id-rsa-minecraft.pub authorized_keys &&
   chmod 600 authorized_keys &&
   mv id-rsa-minecraft .. )
 chown -R minecrft.minecrft .
-wget "$CRAFTBUKKIT_URL"
+wget -O craftbukkit.jar "$CRAFTBUKKIT_URL"
 if [ $? -ne 0 ]
 then
     echo "$PGM: Couldn't download $CRAFTBUKKIT_URL: stopping for now" >&2
     exit 1
 fi
 
-update-rc.d minecraft-server start
+exit 0
